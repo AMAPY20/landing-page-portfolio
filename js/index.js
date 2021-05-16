631d5d1395a572b62aa148338bd2f0166b24cde0
amapy20 <engineer.ama.2021@gmail.com>
2021-05-16 12:42:13 +0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,51 @@
+// على القائمة عند التمرير أكثر من 200 بكسل noTransparrent تطبيق صنف جديد
+$(window).scroll(function () {
+    if ($(this).scrollTop() >= 200) {
+        $("#navBar").addClass("noTransparrent");
+    } else {
+        $("#navBar").removeClass("noTransparrent");
+    }
+});
+
+$(document).ready(function () {
+    $("a.scroll").on('click', function (event) {
+
+        var hash = this.hash;
+
+        $('html, body').animate({ scrollTop: $(hash).offset().top - 90 }, 800, function () { });
+    
+    });
+
+    function checkVisible( elm, eval ) {
+    eval = eval || "visible";
+    var vpH = $(window).height(),
+        st = $(window).scrollTop(),
+        y = $(elm).offset().top,
+        elementHeight = $(elm).height();
+    
+    if (eval == "visible") return ((y < (vpH + st)) && (y > (st - elementHeight)));
+    if (eval == "above") return ((y < (vpH + st)));
+    }
+    var isLoaded = false;
+    
+    $(window).scroll(function () {
+        if (!isLoaded) {
+            if (checkVisible($('#statistics'))) {
+
+                $('.circle').circleProgress({
+                    startAngle: -Math.PI / 2,
+                    fill: "#0575e6",
+                }).on('circle-animation-progress', function (event, progress, stepValue) {
+                    $(this).find('span').html(Math.round(stepValue * 100) + '%');
+                });
+
+                isLoaded = true;
+            }
+        }
+    });
+
+    // emailForm على النموذج validate تطبيق التابع
+    $(function () {
+        $("#commentForm").validate();
+    });
+});
